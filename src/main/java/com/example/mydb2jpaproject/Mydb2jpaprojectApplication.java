7554fcd64d70131de3f848a8aede7352f50ae8a4
subxander <52131178+subxander@users.noreply.github.com>
2020-03-24 14:12:10 -0500

    initial commit

---
+++ src/main/java/com/example/mydb2jpaproject/Mydb2jpaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mydb2jpaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mydb2jpaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mydb2jpaprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mydb2jpaproject/Mydb2jpaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mydb2jpaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mydb2jpaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
