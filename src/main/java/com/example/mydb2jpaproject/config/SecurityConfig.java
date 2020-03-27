642f7ef9294e21c16cd4aa5df2f2d3fcf1e4818d
subxander <52131178+subxander@users.noreply.github.com>
2020-03-27 13:13:59 -0500

    security change allow all for now but added the proper pattern to enable later

--- src/main/java/com/example/mydb2jpaproject/config/SecurityConfig.java
+++ src/main/java/com/example/mydb2jpaproject/config/SecurityConfig.java
@@ -1,31 +1,16 @@
 package com.example.mydb2jpaproject.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
  
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter
-{
-    @Override
-    protected void configure(HttpSecurity http) throws Exception 
-    {
-        http
-         .csrf().disable()
-         .authorizeRequests().anyRequest().authenticated()
-         .and()
-         .httpBasic();
-    }
-  
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) 
-            throws Exception 
-    {
-        auth.inMemoryAuthentication()
-            .withUser("admin")
-            .password("{noop}password")
-            .roles("USER");
-    }
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/*", "/basicAuth").permitAll()
+				.anyRequest().authenticated();
+	}
 }
